05c27864783a445d6b6ce072099db5509c5d46b0
Flaw <flawww65535@outlook.com>
2022-01-22 19:21:56 +0800

    Version 0.1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,23 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
   // Start is called before the first frame update
   public float speed = 0;
+  public TextMeshProUGUI countText;
+  public GameObject winTextObject;
   private Rigidbody rb;
+  private int count;
   private float movementX;
   private float movementY;
   void Start()
   {
+    count = 0;
     rb = GetComponent<Rigidbody>();
+    SetCountText();
+    winTextObject.SetActive(false);
   }
   void OnMove(InputValue movementValue)
   {
     Vector2 movementVector = movementValue.Get<Vector2>();
     movementX = movementVector.x;
     movementY = movementVector.y;
+  }
+  void SetCountText()
+  {
+    countText.text = "Count: " + count.ToString();
+    if (count >= 12)
+    {
+      winTextObject.SetActive(true);
+    }
   }
   void FixedUpdate()
   {
@@ -27,6 +42,11 @@ public class PlayerController : MonoBehaviour
   }
   private void OnTriggerEnter(Collider other)
   {
-    other.gameObject.SetActive(false);
+    if (other.gameObject.CompareTag("PickUp"))
+    {
+      other.gameObject.SetActive(false);
+      count += 1;
+      SetCountText();
+    }
   }
 }
